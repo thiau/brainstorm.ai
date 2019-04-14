c4d72fb25bcd69cefa5199a1a845d987d0b18d88
Thiago Araujo <thirauj@br.ibm.com>
2019-04-14 12:33:26 -0300

    add web client fiels

---
+++ client/main_module/js/components/app.vue
@@ -0,0 +1,33 @@
+<template>
+	<div id="app">
+		<div>OI MANO</div>
+	</div>
+</template>
+
+<script>
+(function() {
+  "use strict";
+
+  module.exports = {
+    props: [],
+    data: function() {
+      return {};
+    },
+    components: {},
+    methods: {},
+    computed: {}
+  };
+})();
+</script>
+
+
+<style scoped>
+#app {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+</style>
+
+
